8631bb6ea52dd2abfb7da2e1aabba87c7cf12a27
divyBhadania <divy.bhadania@bacancy.com>
2023-12-12 17:19:34 +0530

    add different serilog configuration method.

---
+++ SeriLogApiDemo/Controllers/LogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeriLogApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Information")]
+        public IActionResult InformationLog()
+        {
+            _logger.LogInformation("information logged");
+            return Ok();
+        }
+    }
+}
--- SeriLogApiDemo/Program.cs
+++ SeriLogApiDemo/Program.cs
@@ -1,12 +1,48 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System.Configuration;
+using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Serilog Config
+
+//Appsettings.json configuration
+builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
+Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+//Code-based configuration(Inline)
+//method 1
+//builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console(outputTemplate : "[{Timestamp:HH:mm:ss} {Level}] {SourceContext} {Message}{NewLine}{Exception}")
+//                                        .WriteTo.File(path : "Logs/log.txt" 
+//                                                      ,outputTemplate : "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] [{Action}] {Message}{NewLine}{Exception}"
+//                                                      ,rollingInterval : RollingInterval.Day
+//                                                      ,flushToDiskInterval : TimeSpan.FromSeconds(5))
+//                                        .WriteTo.MSSqlServer(connectionString: "Server = DESKTOP-AETEI14\\SQLEXPRESS; Database = SeriLog;Trusted_Connection = true; Encrypt = False"
+//                                        ,sinkOptions: new MSSqlServerSinkOptions()
+//                                                        {
+//                                                            AutoCreateSqlTable = false,
+//                                                            TableName = "seriLog",
+//                                                            BatchPostingLimit = 100,
+//                                                            BatchPeriod = TimeSpan.FromSeconds(10.0),
+//                                                        }
+//                                        ,restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Error));
+
+//method 2
+//Log.Logger = new LoggerConfiguration()
+//            .ReadFrom.Configuration(builder.Configuration)
+//            .CreateLogger();
+//builder.Services.AddLogging(loggingBuilder =>
+//{
+//    loggingBuilder.ClearProviders();
+//    loggingBuilder.AddSerilog();
+//});
+
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,6 +58,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();  // Only if we are using builder.Host method.
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
